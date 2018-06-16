39cc10a782a8b1052100dd1c858dbcbb4777129e
Michal_Ostrega <michalostrega@gmail.com>
2018-06-16 15:35:23 +0200

    NO XML Configuration

--- src/main/java/pl/sdacademy/spring/car_dealer/ApplicationConfiguration.java
+++ src/main/java/pl/sdacademy/spring/car_dealer/ApplicationConfiguration.java
@@ -1,16 +1,15 @@
 package pl.sdacademy.spring.car_dealer;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
+@ComponentScan
 @PropertySource(value = "classpath:application.properties")
 public class ApplicationConfiguration {
-
-
-
 
     //Properties
     @Bean(name = "propertySourcesPlaceholderConfigurer")
--- src/main/java/pl/sdacademy/spring/car_dealer/Main.java
+++ src/main/java/pl/sdacademy/spring/car_dealer/Main.java
@@ -1,11 +1,13 @@
 package pl.sdacademy.spring.car_dealer;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AbstractApplicationContext context =
+                new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
         Application application = context.getBean("application", Application.class);
         application.start();
 
