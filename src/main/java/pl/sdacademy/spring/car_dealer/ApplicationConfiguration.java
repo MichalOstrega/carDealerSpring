cba42bcfde1bc7890f27d2a2522a163cf4f44f89
Michal_Ostrega <michalostrega@gmail.com>
2018-06-23 13:08:52 +0200

    Spring Boot - Enable AutoConfiguration

--- src/main/java/pl/sdacademy/spring/car_dealer/ApplicationConfiguration.java
+++ src/main/java/pl/sdacademy/spring/car_dealer/ApplicationConfiguration.java
@@ -1,6 +1,7 @@
 package pl.sdacademy.spring.car_dealer;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,64 +20,7 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@ComponentScan
-@PropertySource(value = "classpath:application.properties")
-@EnableJpaRepositories
-@EnableTransactionManagement
+@EnableAutoConfiguration
 public class ApplicationConfiguration {
-
-    //Properties
-    @Bean(name = "propertySourcesPlaceholderConfigurer")
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-
-    @Bean
-    public DataSource dataSource(
-            @Value("${spring.datasource.url}") String url,
-            @Value("${spring.datasource.username}") String username,
-            @Value("${spring.datasource.password}") String password,
-            @Value("${spring.datasource.driver-class-name}") String driver) {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        dataSource.setDriverClassName(driver);
-        return dataSource;
-
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-            DataSource dataSource,
-            @Value("${spring.jpa.hibernate.ddl-auto}") String ddlMode,
-            @Value("${spring.jpa.hibernate.dialect}") String dialect,
-            @Value("${spring.jpa.hibernate.connection.characterEncoding}") String encoding) {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource);
-        em.setPackagesToScan("pl.sdacademy.spring");
-        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2dll.auto", ddlMode);
-        properties.setProperty("hibernate.dialect", dialect);
-        properties.setProperty("hibernate.connection.characterEncoding", encoding);
-        em.setJpaProperties(properties);
-
-        return em;
-    }
-
-    @Bean
-    public JpaTransactionManager transactionManager(EntityManagerFactory emf){
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(emf);
-        return transactionManager;
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource){
-        return new JdbcTemplate(dataSource);
-    }
-
 
 }
